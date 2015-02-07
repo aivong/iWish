b575b16dc87a8d0e609ce5946aaec8c3e3547cbb
aivong0101 <aivong0101@gmail.com>
2015-02-07 15:30:21 -0600

    Created new Xcode project for iWish

---
+++ iWishTests/iWishTests.swift
@@ -0,0 +1,36 @@
+//
+//  iWishTests.swift
+//  iWishTests
+//
+//  Created by Ai Vong on 2/7/15.
+//  Copyright (c) 2015 WIS CS428. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class iWishTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
