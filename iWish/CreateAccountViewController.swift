d026b9409f5f282840e18ca658de489517bb8d42
aivong0101 <aivong0101@gmail.com>
2015-02-22 16:27:35 -0600

    Created RegisterAccount View
    Not functional

---
+++ iWish/CreateAccountViewController.swift
@@ -0,0 +1,38 @@
+//
+//  CreateAccountViewController.swift
+//  iWish
+//
+//  Created by Ai Vong on 2/22/15.
+//  Copyright (c) 2015 WIS CS428. All rights reserved.
+//
+
+import UIKit
+
+class CreateAccountViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBAction func registerAccount(sender: UIButton) {
+        println("Hello World")
+    }
+}
