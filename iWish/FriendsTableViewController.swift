9fc60d29656b903cc2e0661e747cef9b519c94aa
Zachary Bohlin <zacharybohlin@aol.com>
2015-02-26 14:44:07 -0600

    Accept and reject friend requests ui added

--- iWish/FriendsTableViewController.swift
+++ iWish/FriendsTableViewController.swift
@@ -13,9 +13,10 @@ class FriendsTableViewController: UITableViewController {
     let headerNames = ["Friend Requests", "Friends"]
     let requests = ["jdog4", "yoohoo443", "yesss99"]
     let friends = ["kbrenc2", "haha7", "justin1", "mitch5", "alex743", "astronaut567", "widewhite82"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
@@ -51,8 +52,8 @@ class FriendsTableViewController: UITableViewController {
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
         if indexPath.section == 0{
-            let cell = tableView.dequeueReusableCellWithIdentifier("RequestCell", forIndexPath:indexPath) as RequestTableViewCell
-            cell.nameLabel?.text = requests[indexPath.row]
+            let cell = tableView.dequeueReusableCellWithIdentifier("RequestCell", forIndexPath:indexPath) as UITableViewCell
+            cell.textLabel?.text = requests[indexPath.row]
             return cell
         }
         else{
@@ -71,25 +72,72 @@ class FriendsTableViewController: UITableViewController {
     }
     
 
-    /*
+    
     // Override to support conditional editing of the table view.
     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return NO if you do not want the specified item to be editable.
-        return true
+        if indexPath.section == 0{
+            return true
+        }
+        else{
+            return false
+        }
     }
-    */
 
-    /*
+
+    
     // Override to support editing the table view.
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+
+        
     }
-    */
+    
+    override func tableView(tableView: UITableView, editActionsForRowAtIndexPath indexPath: NSIndexPath) -> [AnyObject]?  {
+        // 1
+        var acceptAction = UITableViewRowAction(style: UITableViewRowActionStyle.Default, title: "Accept" , handler: { (action:UITableViewRowAction!, indexPath:NSIndexPath!) -> Void in
+            // 2
+            let acceptMenu = UIAlertController(title: nil, message: "Are you sure you want to accept?", preferredStyle: .ActionSheet)
+            
+            let yesActionHandler = { (action:UIAlertAction!) -> Void in
+                let alertMessage = UIAlertController(title: "Accepted!", message: "Not fully implemented yet", preferredStyle: .Alert)
+                alertMessage.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+                self.presentViewController(alertMessage, animated: true, completion: nil)
+            }
+            
+            let yesAction = UIAlertAction(title: "Accept", style: UIAlertActionStyle.Default, handler: yesActionHandler)
+            let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel, handler: nil)
+            
+            acceptMenu.addAction(yesAction)
+            acceptMenu.addAction(cancelAction)
+            
+            
+            self.presentViewController(acceptMenu, animated: true, completion: nil)
+        })
+        // 3
+        var rejectAction = UITableViewRowAction(style: UITableViewRowActionStyle.Default, title: "Reject" , handler: { (action:UITableViewRowAction!, indexPath:NSIndexPath!) -> Void in
+            // 4
+            let rejectMenu = UIAlertController(title: nil, message: "Are you sure you want to reject?", preferredStyle: .ActionSheet)
+            
+            let ryesActionHandler = { (action:UIAlertAction!) -> Void in
+                let alertMessage = UIAlertController(title: "Accepted!", message: "Not fully implemented yet", preferredStyle: .Alert)
+                alertMessage.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+                self.presentViewController(alertMessage, animated: true, completion: nil)
+            }
+            
+            let ryesAction = UIAlertAction(title: "Reject", style: UIAlertActionStyle.Default, handler: ryesActionHandler)
+            let rcancelAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel, handler: nil)
+            
+            rejectMenu.addAction(ryesAction)
+            rejectMenu.addAction(rcancelAction)
+            
+            
+            self.presentViewController(rejectMenu, animated: true, completion: nil)
+        })
+        
+        acceptAction.backgroundColor = UIColor.greenColor()
+        // 5
+        return [rejectAction,acceptAction]
+    }
+    
 
     /*
     // Override to support rearranging the table view.
