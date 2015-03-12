9544186b041a507dda5b420d1efb7728730c1d20
Zachary Bohlin <zacharybohlin@aol.com>
2015-03-12 10:25:58 -0500

    Logout implemented, settings DB needs to be created

--- iWish/SettingsTableViewController.swift
+++ iWish/SettingsTableViewController.swift
@@ -17,28 +17,56 @@ class SettingsTableViewController: UITableViewController {
     @IBOutlet weak var allowSearchSwitch: UISwitch!
 
     @IBAction func notificationSwitchChanged(sender: AnyObject) {
-        println("Notification")
+        let nsud = NSUserDefaults.standardUserDefaults()
+        nsud.setObject(notificationSwitch.on, forKey: "notificationsAllowed")
     }
     
     @IBAction func systemEmailSwitchChanged(sender: AnyObject) {
-        println("System Email")
+        let nsud = NSUserDefaults.standardUserDefaults()
+        nsud.setObject(systemEmailSwitch.on, forKey: "systemEmailsAllowed")
     }
     
     @IBAction func showEmailSwitchChanged(sender: AnyObject) {
-        println("Show Email")
+        let nsud = NSUserDefaults.standardUserDefaults()
+        nsud.setObject(showEmailSwitch.on, forKey: "showEmailAllowed")
     }
     
     @IBAction func showBirthdayChanged(sender: AnyObject) {
-        println("Show Birthday")
+        let nsud = NSUserDefaults.standardUserDefaults()
+        nsud.setObject(showBirthdaySwitch.on, forKey: "showBirthdayAllowed")
     }
     
     @IBAction func allowSearchByUsernameChanged(sender: AnyObject) {
-        println("Show Search")
+        let nsud = NSUserDefaults.standardUserDefaults()
+        nsud.setObject(allowSearchSwitch.on, forKey: "searchUsernameAllowed")
+    }
+    
+    @IBAction func saveSettings(sender: AnyObject) {
+
+    }
+    
+    @IBAction func logout(sender: AnyObject) {
+        self.performSegueWithIdentifier("LogoutSegue", sender: self)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        let nsud = NSUserDefaults.standardUserDefaults()
+        let na = nsud.boolForKey("notificationsAllowed")
+        let sys = nsud.boolForKey("systemEmailsAllowed")
+        let sea = nsud.boolForKey("showEmailAllowed")
+        let sba = nsud.boolForKey("showBirthdayAllowed")
+        let sua = nsud.boolForKey("searchUsernameAllowed")
+        
+        notificationSwitch.on = na
+        systemEmailSwitch.on = sys
+        showEmailSwitch.on = sea
+        showBirthdaySwitch.on = sba
+        allowSearchSwitch.on = sua
+        
+        
+        println(na)
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
@@ -49,6 +77,12 @@ class SettingsTableViewController: UITableViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+        println("Leaving Settings")
     }
 
     
