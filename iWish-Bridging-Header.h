d9fd32f59d4ac3ee59425e084e245c4c0dbc4127
aivong0101 <aivong0101@gmail.com>
2015-04-12 22:22:18 -0500

    Registration Email now sent
    Need to be merged to master branch

---
+++ iWish-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  iWish-Bridging-Header.h
+//  iWish
+//
+//  Created by Ai Vong on 4/12/15.
+//  Copyright (c) 2015 WIS CS428. All rights reserved.
+//
+//
+//#ifndef iWish_iWish_Bridging_Header_h
+//#define iWish_iWish_Bridging_Header_h
+//
+//
+//#endif
+#import <MailCore/MailCore.h>
